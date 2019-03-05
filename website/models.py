5ef68995641451908f70096a8609d0084a0a5d7e
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2019-03-05 11:15:06 -0600

    Set up models

--- website/models.py
+++ website/models.py
@@ -1,13 +1,41 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
-class Product(models.Model):
-    seller = models.ForeignKey(
+class Score(models.Model):
+    value = models.IntegerField(max_length=3)
+
+class Goal(models.Model):
+    description = models.CharField(max_length=250)
+
+class GradeLevel(models.Model):
+    grade = models.CharField(max_length=1)
+
+class Student(models.Model):
+    user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
     )
-    title = models.CharField(max_length=255)
-    description = models.TextField(blank=True, null=True)
-    price = models.IntegerField()
-    quantity = models.IntegerField()
+    gradeLevel = models.ForeignKey(
+        GradeLevel,
+        on_delete=models.CASCADE,
+    )
+    firstName = models.CharField(max_length=50)
+    lastName = models.CharField(max_length=50)
+    age = models.IntegerField()
+    classroomTeacher = models.CharField(max_length=50)
+
+class Evaluation(models.Model):
+    student = models.ForeignKey(
+        Student,
+        on_delete=models.CASCADE,
+    )
+    score = models.ForeignKey(
+        Score,
+        on_delete=models.CASCADE,
+    )
+    goal = models.ForeignKey(
+        Goal,
+        on_delete=models.CASCADE,
+    )
+    date = models.DateField()
+    schoolWeek = models.IntegerField(max_length=2)
