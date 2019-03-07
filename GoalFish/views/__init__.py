932f7390ce6dd8930d7d3788eb81a350b7bcd14f
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2019-03-07 12:14:52 -0600

    Add functionality to view student details at the indvidual student detail page and linked each on from the all students page

--- GoalFish/views/__init__.py
+++ GoalFish/views/__init__.py
@@ -1,3 +1,2 @@
-# from .departmentDetail_view import detail
 from .auth import index, register, login_user, user_logout
 from .student import list_students, display_student_form, add_student, student_detail
